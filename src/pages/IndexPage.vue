d6fd8ba3e1d2c44778f0241431970345946fcf38
Poomicon <whphone154@gmail.com>
2024-07-31 16:11:51 +0700

    Merge branch 'thai-lang'

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -33,7 +33,16 @@
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type your surname']"
       />
-
+      <!-- language -->
+      <q-input
+        filled
+        v-model="language"
+        label="ภาษา *"
+        hint="ภาษา"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'กรุณาใส่ภาษา']"
+      />
+      <!-- age -->
       <q-input
         filled
         type="number"
@@ -67,6 +76,7 @@ export default {
     const id = ref("6604101329")
     const name = ref("ณัฐภูมิ")
     const surname = ref("หลู่จิ่ง")
+    const language = ref("ภาษาไทย")
     const age = ref(null)
     const accept = ref(false)
 
@@ -74,6 +84,7 @@ export default {
       id,
       name,
       surname,
+      language,
       age,
       accept,
 
@@ -102,6 +113,7 @@ export default {
         name.value = null
         age.value = null
         accept.value = false
+        language.value = null
       }
     }
   }
