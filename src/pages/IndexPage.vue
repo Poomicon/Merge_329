939630c15ade420e2431a6b43097f88c2b40feb7
Poomicon <whphone154@gmail.com>
2024-07-31 10:40:50 +0700

    modified id-code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,6 +6,15 @@
       @reset="onReset"
       class="q-gutter-md"
     >
+     <!-- id -->
+     <q-input
+        filled
+        v-model="id"
+        label="Your id *"
+        hint="id-code"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type your id-code']"
+      />
       <!-- name -->
       <q-input
         filled
@@ -55,13 +64,14 @@ import { ref } from 'vue'
 export default {
   setup () {
     const $q = useQuasar()
-
+    const id = ref("6604101329")
     const name = ref("ณัฐภูมิ")
     const surname = ref("หลู่จิ่ง")
     const age = ref(null)
     const accept = ref(false)
 
     return {
+      id,
       name,
       surname,
       age,
